576f25c6dd3df7257634d993f35574e67a72171b
Mar <margomgar35@alu.edu.gva.es>
2025-01-29 17:45:35 +0100

    initial commit

--- src/main/kotlin/com/example/segonspringmvc/controller/ComarcaController.kt
+++ src/main/kotlin/com/example/segonspringmvc/controller/ComarcaController.kt
@@ -2,13 +2,23 @@ package com.example.segonspringmvc.controller
 
 import com.example.segonspringmvc.model.comarca
 import com.example.segonspringmvc.repository.ComarcaRepository
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping("/comarca")
+//@RestController
+@Controller
+@RequestMapping("/comarcas")
 class ComarcaController (private val comarcaRepository: ComarcaRepository) {
+//    @GetMapping
+//    fun obtenerComarcas(): List<comarca> = comarcaRepository.findAll()
+
     @GetMapping
-    fun obtenerComarcas(): List<comarca> = comarcaRepository.findAll()
+    fun listarComarcas(model: Model): String {
+        val comarcas = comarcaRepository.findAll()
+        model.addAttribute("comarcas", comarcas)
+        return "comarcas"
+    }
 }
