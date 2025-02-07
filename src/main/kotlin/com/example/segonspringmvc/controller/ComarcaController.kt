0d87685c40b61713e4d00b7f4db8d2cc78a600d7
Mar <margomgar35@alu.edu.gva.es>
2025-02-07 20:48:05 +0100

    crud

--- src/main/kotlin/com/example/segonspringmvc/controller/ComarcaController.kt
+++ src/main/kotlin/com/example/segonspringmvc/controller/ComarcaController.kt
@@ -4,10 +4,7 @@ import com.example.segonspringmvc.model.comarca
 import com.example.segonspringmvc.repository.ComarcaRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 //@RestController
 @Controller
@@ -16,12 +13,12 @@ class ComarcaController (private val comarcaRepository: ComarcaRepository) {
 //    @GetMapping
 //    fun obtenerComarcas(): List<comarca> = comarcaRepository.findAll()
 
-//    @GetMapping
-//    fun listarComarcas(model: Model): String {
-//        val comarcas = comarcaRepository.findAll()
-//        model.addAttribute("comarcas", comarcas)
-//        return "/vistas/comarcas"
-//    }
+    @GetMapping
+    fun listarComarcas(model: Model): String {
+        val comarcas = comarcaRepository.findAll()
+        model.addAttribute("comarcas", comarcas)
+        return "/vistas/comarcas"
+    }
 
 //    @GetMapping("/buscar")
 //    fun buscarComarcasPorProvincia(@RequestParam provincia: String): List<comarca> {
@@ -33,5 +30,33 @@ class ComarcaController (private val comarcaRepository: ComarcaRepository) {
         val comarcas = comarcaRepository.findComarcasPorProvincia(provincia)
         model.addAttribute("comarcas", comarcas)
         return "/vistas/comarcas"
+    }
+
+    @GetMapping("/nueva")
+    fun mostrarFormularioNuevaComarca(model: Model): String {
+        model.addAttribute("comarca", comarca())
+        return "/vistas/nueva-comarca"
+    }
+
+    @PostMapping("/nueva")
+    fun guardarComarca(@ModelAttribute comarca: comarca): String {
+        comarcaRepository.save(comarca)
+        return "redirect:/comarcas"
+    }
+
+    @GetMapping("/editar/{id}")
+    fun mostrarFormularioComarca(@PathVariable id: String, model: Model): String {
+        val comarca = comarcaRepository.findById(id).orElseThrow { IllegalArgumentException("Comarca no encontrada: $id") }
+        model.addAttribute("comarca", comarca)
+        return "editar-comarcas"
+    }
+
+    @PostMapping("/editar{id}")
+    fun actualizarComarca(@PathVariable id: String, @ModelAttribute comarca: comarca): String {
+        if (!comarcaRepository.existsById(id)) {
+            throw IllegalArgumentException("Comarca no encontrada: $id")
+        }
+        comarcaRepository.save(comarca)
+        return "redirect:/comarcas"
     }
 }
